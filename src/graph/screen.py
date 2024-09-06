20eac27142775ea7667e908f2902a7be42d0b5c0
elaurentium <credopuke@icloud.com>
2024-09-06 11:09:28 -0300

    adicionando novo botão e estilizando

--- src/graph/screen.py
+++ src/graph/screen.py
@@ -9,16 +9,26 @@ def onSelection(event):
 def screen():
     root = tk.Tk()
     root.title("EXECUTOR SCRIPT")
-    root.geometry("300x100")
+    root.geometry("300x180")
+
+    style = ttk.Style()
+    style.configure('TButton', padding=10)
+    style.configure('TCombox', padding=5)
 
     options = ["Script Completo", "Atualização", "XPTO"]
     input_options = ttk.Combobox(root, values=options)
-    input_options.pack(pady=10)
+    input_options.pack(pady=20, padx=10)
 
     input_options.bind("<<ComboboxSelected>>", onSelection)
 
-    button = ttk.Button(root, text="Executar", command=lambda: execButton(select))
-    button.pack(pady=10)
+    button_frame = tk.Frame(root)
+    button_frame.pack(side='right', padx=10)
 
+    browserButton = ttk.Button(button_frame, text="Procurar")
+    browserButton.pack(side='top', pady=5)
+
+    button = ttk.Button(button_frame, text="Executar", command=lambda: execButton(select))
+    button.pack(side='top', pady=5)
+    
     root.mainloop()
 
