7f574659a540e5ad8619c4c9155b30881b1f6294
elaurentium <credopuke@icloud.com>
2024-11-03 21:39:45 -0300

    nha

--- src/gui/screen.py
+++ src/gui/screen.py
@@ -1,23 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
-import rust_python_integration
+import json
+import os
+from controller.controlScript import *
 
-options = rust_python_integration.load_paths()
+PATH_FILE = r'.\src\paths\paths.json' if os.name == 'nt' else './src/paths/paths.json'
+
+def load_paths():
+    if os.path.exists(PATH_FILE):
+        with open(PATH_FILE, 'r') as file:
+            return json.load(file)
+    return []
+
+def save_path(paths):
+    os.makedirs(os.path.dirname(PATH_FILE), exist_ok=True)
+    with open(PATH_FILE, 'w') as file:
+        json.dump(paths,file)
+
+options = load_paths()
 
 def onSelection(event):
     global select
     select = event.widget.get()
 
+
 def updateOptions(option):
-    new_option = rust_python_integration.search_button()
+    new_option = search_button()
 
     options.append(new_option)
 
     new_val = list(set(options))
 
     option['values'] = new_val
-    
-    rust_python_integration.save_path(new_val)
+
+    save_path(new_val)
 
 def screen():
     root = tk.Tk()
@@ -39,7 +55,9 @@ def screen():
     browserButton = ttk.Button(button_frame, text="Procurar", command=lambda: updateOptions(input_options))
     browserButton.pack(side='top', pady=5)
 
-    button = ttk.Button(button_frame, text="Executar", command=lambda: rust_python_integration.exec_button(select))
+    button = ttk.Button(button_frame, text="Executar", command=lambda: controlScript.exec_button(select))
     button.pack(side='top', pady=5)
     
     root.mainloop()
+
+
